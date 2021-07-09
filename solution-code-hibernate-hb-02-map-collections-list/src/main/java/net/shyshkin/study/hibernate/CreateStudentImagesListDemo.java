78078d0ccd2c3c1b091c53ce6b85b2f268b42238
Art <d.art.shishkin@gmail.com>
2021-07-09 14:56:47 +0300

    12. Mappings Lists - Write Some Code (#1)

---
+++ solution-code-hibernate-hb-02-map-collections-list/src/main/java/net/shyshkin/study/hibernate/CreateStudentImagesListDemo.java
@@ -0,0 +1,48 @@
+package net.shyshkin.study.hibernate;
+
+import net.shyshkin.study.hibernate.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class CreateStudentImagesListDemo {
+    public static void main(String[] args) {
+
+        Transaction transaction = null;
+
+        // create session factory
+
+        // create session
+        try (SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+             Session session = factory.getCurrentSession()) {
+
+            // create the object
+            Student student = new Student("Art", "Shyshkin", "art@example.com");
+            student.getImages().addAll(List.of("image01.jpg", "image02.jpg", "image03.jpg", "image04.jpg", "image04.jpg"));
+
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // save the object
+            System.out.println("Saving the student and images");
+            session.persist(student);
+
+            // commit the transaction
+            transaction.commit();
+
+        } catch (Exception exception) {
+            // clean up code
+            if (transaction != null) transaction.rollback();
+        }
+
+    }
+}
---
+++ solution-code-hibernate-hb-02-map-collections-list/src/test/java/net/shyshkin/study/hibernate/CreateStudentImagesListDemoTest.java
@@ -0,0 +1,21 @@
+package net.shyshkin.study.hibernate;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple CreateStudentImagesListDemo.
+ */
+public class CreateStudentImagesListDemoTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
