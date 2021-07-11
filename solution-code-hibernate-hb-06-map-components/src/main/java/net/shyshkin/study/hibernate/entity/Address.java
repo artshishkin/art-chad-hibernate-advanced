b514d6db17709cbea8a0797ce6115f287df6a52b
Art <d.art.shishkin@gmail.com>
2021-07-11 10:35:06 +0300

    21. Embeddable - Develop an Embedded Component (#3 Section 4: Mapping Components - Embeddable)

---
+++ solution-code-hibernate-hb-06-map-components/src/main/java/net/shyshkin/study/hibernate/entity/Address.java
@@ -0,0 +1,69 @@
+package net.shyshkin.study.hibernate.entity;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    private String street;
+    private String city;
+    private String zipcode;
+
+    public Address() {
+    }
+
+    public Address(String street, String city, String zipcode) {
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(street, address.street) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(street, city, zipcode);
+    }
+}
